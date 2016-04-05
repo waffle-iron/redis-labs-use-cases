71250e983bc583940a124d2afda2275c46598d25
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-05 14:33:47 -0300

    twitter stream: Adding cors to express api
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- twitter-stream/express-api/app.js
+++ twitter-stream/express-api/app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require('cors');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 var routes = require("./routes/routes.js")(app);
 
