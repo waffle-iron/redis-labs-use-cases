c482692434e268a1dca96b80d16f380174850ca8
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-07 10:27:58 -0300

    twitter stream api: add envs for twitter credentials
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- twitter-stream/express-api/config.js
+++ twitter-stream/express-api/config.js
@@ -15,10 +15,10 @@ config.redis = {
 
 config.twitter =
 {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    consumer_key: process.env.CONSUMER_KEY || '',
+    consumer_secret: process.env.CONSUMER_SECRET || '',
+    access_token_key: process.env.ACCESS_TOKEN_KEY || '',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET || ''
 };
 
 config.store = {
