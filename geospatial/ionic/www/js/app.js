44a61eab65a7789547c1779ed08c7307016d1e58
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-21 11:04:37 -0300

    geospatial: changes to deploy with ionic view
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- geospatial/ionic/www/js/app.js
+++ geospatial/ionic/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.filters', 'ngStorage', 'uuid4', 'ngLodash', 'uiGmapgoogle-maps', 'ngCordova', 'ngMessages'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.filters', 'ngStorage', 'uuid4', 'ngLodash', 'uiGmapgoogle-maps', 'ngCordova', 'ngMessages', 'ionic.service.core'])
 
 .run(function($ionicPlatform, $rootScope, $localStorage, uuid4) {
 
