98e47dcde4a8bf29db26ca205e6781dc9f30ab1d
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-25 13:35:57 -0300

    geospatial: api addLocation
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- geospatial/express-api/routes/routes.js
+++ geospatial/express-api/routes/routes.js
@@ -73,6 +73,18 @@ var customLogin = function(req, res, next) {
 
 };
 
+var addLocation = function(req, res, next) {
+  if(!req.query.name || !req.query.latitude || !req.query.longitude) {
+    return res.json({"status" : "error", "message" : 'No name, latitude or longitude provided.'});
+  }
+
+  backend.addLocation(req.query.name, req.query.latitude, req.query.longitude, req.user)
+    .then( function(result) { res.json({"status" : "success", "result" : result }); })
+    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
+};
+
+
+
 var appRouter = function(app) {
   app.get('/', rootFunc);
   app.use(customLogin);
@@ -81,6 +93,7 @@ var appRouter = function(app) {
   app.get('/findpos/', findLocationPos);
   app.get('/members/', findMembers);
   app.get('/radiuses/', findRadiuses);
+  app.get('/add/', addLocation);
 };
 
 module.exports = appRouter;
