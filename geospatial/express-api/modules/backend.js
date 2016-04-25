753bba6666a673c75a7c53f15553890458ff5302
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-25 15:39:54 -0300

    killing code for workshop base
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- geospatial/express-api/modules/backend.js
+++ geospatial/express-api/modules/backend.js
@@ -15,17 +15,9 @@ redis.on("error", function (err) {
 });
 
 redis.on("ready", function () {
-  var addLocation = function(city)  {
-    exports.addLocation(city.city, city.lon, city.lat);
-  };
-
   var version = redis.server_info.redis_version;
   if(version[0] <= 3 && version[1] < 1) {
     console.log('Error: Must have redis >= 3.2');
-  }
-
-  if(config.addDefaultValues) {
-    _.forEach(cities, addLocation);
   }
 });
 
@@ -56,91 +48,6 @@ exports.findUser = function(token) {
       });
     } else {
       deferred.resolve(reply);
-    }
-  });
-
-  return deferred.promise;
-};
-
-exports.addLocation = function(location, lng, lat, userId) {
-  var dfd = Q.defer();
-  var locationGeoSet = config.store.locationGeoSet;
-  var geoArgs = [ locationGeoSet, lng, lat, location ];
-  redis.geoadd(geoArgs, function (err, reply) {
-    if(err) {
-      dfd.reject(err);
-      return;
-    }
-    return dfd.resolve(reply);
-  });
-
-  return dfd.promise;
-};
-
-
-exports.findLocationsByMember = function(member, radius, unit, userId) {
-  var locationGeoSet = config.store.locationGeoSet;
-  var geoArgs = [ locationGeoSet, member, radius, unit ];
-  var deferred = Q.defer();
-
-  redis.georadiusbymember(geoArgs, function(err, response) {
-    if(err) {
-      deferred.reject(err);
-    } else {
-      deferred.resolve(response);
-    }
-  });
-
-  return deferred.promise;
-};
-
-exports.findLocationsByCoords = function(long, lat, radius, unit, userId) {
-  var locationGeoSet = config.store.locationGeoSet;
-  var geoArgs = [ locationGeoSet, long, lat, radius, unit, 'WITHCOORD' ];
-  var deferred = Q.defer();
-
-  redis.georadius(geoArgs, function(err, response) {
-    if(err) {
-      deferred.reject(err);
-    } else {
-      var col = _.map(response, function(v,j) { return { name: v[0], long: v[1][0], lat: v[1][1] }; });
-      deferred.resolve(col);
-    }
-  });
-
-  return deferred.promise;
-};
-
-exports.findLocationPos = function(member, userId) {
-  var locationGeoSet = config.store.locationGeoSet;
-  var geoArgs = [ locationGeoSet, member ];
-  var deferred = Q.defer();
-
-  redis.geopos(geoArgs, function(err, response) {
-    if(err) {
-      deferred.reject(err);
-    } else {
-      var obj = {};
-      if(_.every(response, Boolean)) {
-        obj = { name: member, long: response[0][0], lat: response[0][1] };
-      }
-      deferred.resolve(obj);
-    }
-  });
-
-  return deferred.promise;
-};
-
-exports.findMembers = function(userId) {
-  var locationGeoSet = config.store.locationGeoSet;
-  var geoArgs = [ locationGeoSet, 0, -1 ];
-  var deferred = Q.defer();
-
-  redis.zrange(geoArgs, function(err, response) {
-    if(err) {
-      deferred.reject(err);
-    } else {
-      deferred.resolve(response);
     }
   });
 
--- geospatial/express-api/routes/routes.js
+++ geospatial/express-api/routes/routes.js
@@ -4,57 +4,6 @@ var rootFunc = function(req, res) {
   res.send("Geospatial: Express API");
 };
 
-var findLocationsByMember = function(req, res, next) {
-  if(!req.query.radius || !req.query.member) {
-    return res.json({"status" : "error", "message" : 'No radius or member provided.'});
-  }
-
-  var values = req.query.radius.split(' ');
-  if(values.length != 2) {
-    return res.json({"status" : "error", "message" : 'Wrong radius format.'});
-  }
-
-  backend.findLocations(req.query.member, values[0], values[1], req.user)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findLocationsByCoords = function(req, res, next) {
-  if(!req.query.radius || !req.query.lat || !req.query.long) {
-    return res.json({"status" : "error", "message" : 'No radius or lat or long provided.'});
-  }
-
-  var values = req.query.radius.split(' ');
-  if(values.length != 2) {
-    return res.json({"status" : "error", "message" : 'Wrong radius format.'});
-  }
-
-  backend.findLocationsByCoords(req.query.long, req.query.lat, values[0], values[1], req.user)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findLocationPos = function(req, res, next) {
-  if(!req.query.member) {
-    return res.json({"status" : "error", "message" : 'No member provided.'});
-  }
-
-  backend.findLocationPos(req.query.member, req.user)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findRadiuses = function(req, res, next) {
-  var radiuses = backend.findRadiuses();
-  return res.json({"status" : "success", "result" : radiuses });
-};
-
-var findMembers = function(req, res, next) {
-  backend.findMembers(req.user)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
 var customLogin = function(req, res, next) {
 
   var token = req.headers['x-access-token'];
@@ -73,27 +22,15 @@ var customLogin = function(req, res, next) {
 
 };
 
-var addLocation = function(req, res, next) {
-  if(!req.query.name || !req.query.latitude || !req.query.longitude) {
-    return res.json({"status" : "error", "message" : 'No name, latitude or longitude provided.'});
-  }
-
-  backend.addLocation(req.query.name, req.query.latitude, req.query.longitude, req.user)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
+var findRadiuses = function(req, res, next) {
+  var radiuses = backend.findRadiuses();
+  return res.json({"status" : "success", "result" : radiuses });
 };
-
-
 
 var appRouter = function(app) {
   app.get('/', rootFunc);
   app.use(customLogin);
-  app.get('/findbycoords/', findLocationsByCoords);
-  app.get('/findbymember/', findLocationsByMember);
-  app.get('/findpos/', findLocationPos);
-  app.get('/members/', findMembers);
   app.get('/radiuses/', findRadiuses);
-  app.get('/add/', addLocation);
 };
 
 module.exports = appRouter;
