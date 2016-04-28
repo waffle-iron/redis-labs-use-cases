7d397ed424bf215f3fee56b44fbcc15a3a16ace9
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-28 10:32:26 -0300

    twitter workshop base
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- twitter-stream/express-api/routes/routes.js
+++ twitter-stream/express-api/routes/routes.js
@@ -4,88 +4,6 @@ var rootFunc = function(req, res) {
   res.send("Twitter Stream: Express API");
 };
 
-var findRecommendations = function(req, res, next) {
-  backend.findRecommendations(req.user, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findLikes = function(req, res, next) {
-  backend.findLikes(req.user, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-
-var findToSwipe = function(req, res, next) {
-  backend.findToSwipe(req.user, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findViewed = function(req, res, next) {
-  var offset = 0;
-  var qty_per_page = 10;
-
-  if(req.query.page) {
-    offset = (parseInt(req.query.page)-1) * qty_per_page;
-  }
-
-  if(offset <= 0) {
-    offset = 0;
-  }
-
-  backend.findViewed(req.user, offset, qty_per_page, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var findByHashtag = function(req, res, next) {
-  var offset = 0;
-  var qty_per_page = 10;
-
-  if(req.query.page) {
-    offset = (parseInt(req.query.page)-1) * qty_per_page;
-  }
-
-  if(offset <= 0) {
-    offset = 0;
-  }
-
-  console.log("Searching", req.params.hashtag, offset, qty_per_page, req.params.channel);
-  backend.findByHashtag(req.params.hashtag, offset, qty_per_page, req.user, req.params.channel)
-    .then(function(result){
-      res.json({"status" : "success", "result" : result });
-    })
-    .fail(function(err){
-      console.error("Error", err);
-      res.json({"status" : "error", "message" : err});
-    });
-};
-
-var findById = function(req, res, next) {
-  backend.findById(req.params.tweet, req.user, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-
-var nopeTweet = function(req, res, next) {
-  backend.nopeTweet(req.params.tweet, req.user, req.params.channel)
-    .then( function(result) { res.json({"status" : "success", "result" : result }); })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
-var likeTweet = function(req, res, next) {
-  backend.voteTweet(req.params.tweet, req.user, req.params.channel)
-    .then(function(result) {
-      backend.likeTweet(req.params.tweet, req.user, req.params.channel)
-      .then( function(result) { res.json({"status" : "success", "result" : result }); })
-      .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-    })
-    .fail( function(err) { res.json({"status" : "error", "message" : err}); });
-};
-
 var customLogin = function(req, res, next) {
   var token = req.headers['x-access-token'];
   if(!token) {
@@ -110,14 +28,6 @@ var getChannels = function(req, res, next) {
 var appRouter = function(app) {
   app.use(customLogin);
   app.get('/', rootFunc);
-  app.get('/hashtag/hashtag/:channel', findByHashtag);
-  app.get('/viewed/:channel', findViewed);
-  app.get('/tweet/:tweet/:channel', findById);
-  app.get('/like/:tweet/:channel', likeTweet);
-  app.get('/likes/:channel', findLikes);
-  app.get('/swipes/:channel', findToSwipe);
-  app.get('/nope/:tweet/:channel', nopeTweet);
-  app.get('/recommendations/:channel', findRecommendations);
   app.get('/channels/', getChannels);
 };
 
