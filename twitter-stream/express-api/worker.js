fc21c4c68dc53bf54afb244a11ae0dc41e8698dc
José Luis Di Biase <josx@interorganic.com.ar>
2016-04-28 10:33:37 -0300

    twitter workshop worker
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- twitter-stream/express-api/worker.js
+++ twitter-stream/express-api/worker.js
@@ -22,3 +22,55 @@ redis.on("error", function (err) {
 
 
 console.log("Initiating Streaming from", config.app.channels);
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+var twitterSubscribe  = function(channel) {
+  client.stream('statuses/filter', {track: channel, lang: 'en'},  function(stream) {
+    stream.on('data', function(tweet) {
+      var tweetHashChannel = config.store.tweetHash + ':' + channel;
+      var tweetSetChannel = config.store.tweetSet + ':' + channel;
+      var voteZsetChannel = config.store.voteZset + ':' + channel;
+      var hashtagZsetChannel = config.store.hashtagZset + ':' + channel;
+
+      console.log(JSON.stringify(channel, tweet.text, null, 4));
+
+      //Hash with tweet text
+      redis.hset(tweetHashChannel, tweet.id_str, tweet.text);
+
+      //Set with tweets id
+      redis.sadd(tweetSetChannel, tweet.id_str);
+
+      //Preparing zset vote index
+      var tVote = [ voteZsetChannel, 0, tweet.id_str ];
+      redis.zadd(tVote);
+
+      var args = [ hashtagZsetChannel ];
+      async.forEach(twitterText.extractHashtags(tweet.text),
+        function (hashtag, callback) {
+          console.log("Found hashtag ", hashtag);
+          args.push(stringHash(hashtag));
+          args.push(tweet.id_str);
+          callback();
+        },
+        function (err) {
+          console.log(JSON.stringify(args, null, 4));
+          redis.zadd(args, function (err, response) {
+            if (err) {
+              console.error("ZADD error ", err);
+              return;
+            }
+          });
+        }
+      );
+    });
+
+    stream.on('error', function(error) {
+        console.log(error);
+    });
+  });
+};
+
+_.mapKeys(config.app.channels, twitterSubscribe);
